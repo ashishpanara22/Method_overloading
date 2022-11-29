945533a2ad9f3b35a077f10956e752e701fef43b
ashishpanara22 <ashish.wappnet@gmail.com>
2022-11-29 10:17:18 +0530

    method overloading

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Method_overloading
+# Method_Overloading
+
+## Overloading is the ability of a function or an operator to behave in different ways based on the parameters that are passed to the function, or the operands that the operator acts on
+
+### step 1 : import multipledispatch which help to solve overloading problem
+
+### step 2 : create main class which name is Calc
+
+### step 3 : create decorater which name is dispatch
+
+### step 4 : create two function with same name and different parameter
+
+### step 5 : create object of Calc class
+
+### step 6 : call function using object
--- method_overloading.py
+++ method_overloading.py
@@ -1,12 +1,15 @@
-class Calc(): # main class or parent class
+from multipledispatch import dispatch
+class Calc(): # main class
 
     # same method in same class
     
+    @dispatch(int,int) # solving overloading problem
     def Sum(self,num1,num2):
         self.num1 = num1
         self.num2 = num2
         print(self.num1+self.num2)
 
+    @dispatch(int,int,int)
     def Sum(self,num1,num2,num3):
         self.num1 = num1
         self.num2 = num2
@@ -15,5 +18,5 @@ class Calc(): # main class or parent class
 
 sum = Calc() # object of Calc class
 
+sum.Sum(1,2) # output is 3
 sum.Sum(1,2,3) # output is 6
-sum.Sum(1,2) # this output produced error because there are same method in same class it called method overloading
